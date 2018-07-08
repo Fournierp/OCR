fdb82a055999976598b8f0f0b7efd2e639525d5c
Fournierp <paul.fournier4@mail.mcgill.ca>
2018-07-07 21:30:10 -0400

    Separate words in images in folder

--- Word Recognition/separate_letters.py
+++ Word Recognition/separate_letters.py
@@ -1,91 +1,68 @@
+import os
+import os.path
+import glob
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-import imutils
 
-#Load image and convert to grey.
-img = cv2.imread('samples/2cegf.png', 0)
+letter_contours = []
+counts = {}
 
-# From RGB to BW
-# Adaptive thresholding
-# blur = cv2.blur(img,(5,5),0)
-th = cv2.adaptiveThreshold(
-    img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 17, 2)
-
-# Otsu thresholding
-ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-# Otsu thresholding with Gaussian Blur
-blur = cv2.GaussianBlur(img,(5,5),0)
-ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-#Plot
-titles = ['Threshold', 'Adaptive', 'Otsu', 'Gaussian + Otsu']
-images = [img, th, th2, th3]
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
-plt.show()
-
-#Erode and dilate (Because it is black on white, erosion dilates and dilation erodes).
-kernel = np.ones((3,3), np.uint8)
-dilation = cv2.dilate(th, kernel, iterations=1)
-dilation2 = cv2.dilate(th2, kernel, iterations=1)
-dilation3 = cv2.dilate(th3, kernel, iterations=1)
-
-titles2 = ['Dilation', 'Adaptive', "Otsu", 'Gaussian + Otsu']
-images2 = [img, dilation, dilation2, dilation3]
-
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images2[i], 'gray')
-    plt.title(titles2[i])
-    plt.xticks([]), plt.yticks([])
-
-plt.show()
-
-erosion = cv2.erode(dilation, kernel, iterations=1)
-erosion2 = cv2.erode(dilation2, kernel, iterations=1)
-erosion3 = cv2.erode(dilation3, kernel, iterations=1)
-
-titles3 = ['Erosion', 'Adaptive', "Otsu", 'Gaussian + Otsu']
-images3 = [img, erosion, erosion2, erosion3]
-
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images3[i], 'gray')
-    plt.title(titles3[i])
-    plt.xticks([]), plt.yticks([])
-
-plt.show()
-
-kernel = np.ones((3,1), np.uint8)
-dilation = cv2.dilate(erosion, kernel, iterations=1)
-dilation2 = cv2.dilate(erosion2, kernel, iterations=1)
-dilation3 = cv2.dilate(erosion3, kernel, iterations=1)
-
-titles4 = ['Dilation', 'Adaptive', "Otsu", 'Gaussian + Otsu']
-images4 = [img, dilation, dilation2, dilation3]
-
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images4[i], 'gray')
-    plt.title(titles4[i])
-    plt.xticks([]), plt.yticks([])
-
-plt.show()
-
-#Get the individual letters.
+#After the trials done in the notebook we can get the contours of each letters.
 x, y, w, h = 30, 12, 20, 38
 for  i in range(5):
-    # get the bounding rect
-    cv2.rectangle(dilation, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    letter_contours.append((x, y, w, h))
     x += w
 
-titles3 = ['Contouring', 'Adaptive', "Otsu", 'Gaussian + Otsu']
-images3 = [img, dilation, dilation2, dilation3] #img, opening, opening2, opening3]
+# Image folders.
+CAPTCHA_IMAGE_FOLDER = "samples"
+OUTPUT_FOLDER = "letters"
 
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images3[i], 'gray')
-    plt.title(titles3[i])
-    plt.xticks([]), plt.yticks([])
+# List of captchas.
+captcha_images = glob.glob(os.path.join(CAPTCHA_IMAGE_FOLDER, "*"))
 
-plt.show()
+# Go through each image in the folder.
+for (i, captcha_image) in enumerate(captcha_images):
+    print("Image {}/{}".format(i + 1, len(captcha_image)))
+
+    # Get the letter labels from the imgae names.
+    filename = os.path.basename(captcha_image)
+    captcha_correct_text = os.path.splitext(filename)[0]
+
+    #Load image and convert to grey.
+    img = cv2.imread(captcha_image, 0)
+
+    # From RGB to BW
+    # Adaptive thresholding
+    th = cv2.adaptiveThreshold(
+        img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 17, 2)
+
+    #Erode and dilate (Because it is black on white, erosion dilates and dilation erodes).
+    kernel = np.ones((3,3), np.uint8)
+    dilation = cv2.dilate(th, kernel, iterations=1)
+
+    erosion = cv2.erode(dilation, kernel, iterations=1)
+
+    kernel = np.ones((3,1), np.uint8)
+    dilation = cv2.dilate(erosion, kernel, iterations=1)
+
+    for letter_contour, letter_text in zip(letter_contours, captcha_correct_text):
+        # Grab the coordinates of the letter in the image.
+        x, y, w, h = letter_contour
+
+        # Extract the letter from the original image with a 2-pixel margin around the edge.
+        letter_image = dilation[y - 2:y + h + 2, x - 2:x + w + 2]
+
+        # Get the folder to save the image in
+        save_path = os.path.join(OUTPUT_FOLDER, letter_text)
+
+        # If the output directory does not exist, create it
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # Write the letter image to a file
+        count = counts.get(letter_text, 1)
+        p = os.path.join(save_path, "{}.png".format(str(count).zfill(6)))
+        cv2.imwrite(p, letter_image)
+
+        # increment the count for the current key
+        counts[letter_text] = count + 1
